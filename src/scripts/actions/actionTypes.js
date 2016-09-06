569a082eb16a015fca604a69b41e35213efa8a35
Go7hic <gtfx0209@qq.com>
2016-09-06 22:26:47 +0800

    update

--- src/scripts/actions/actionTypes.js
+++ src/scripts/actions/actionTypes.js
@@ -1,9 +1,17 @@
-import { generateAction } from 'js/utils/actions'
-const actions = ['NEW_TILE', 'MOVE_TILES', "ACTUALIZE",
+import {generateActions} from "js/utils/actions";
+
+/**
+ * Actions to be generated.
+ */
+const actions = [
+  "NEW_TILE",
+  "MOVE_TILES",
+  "ACTUALIZE",
   "MERGE_TILES",
   "INIT_GAME",
   "GAME_OVER",
   "SAVE_GAME",
   "RESET_RESULT"
-]
-export default generateActions(actions)
+];
+
+export default generateActions(actions);
